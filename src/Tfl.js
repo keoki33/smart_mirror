0b0df751c37826fc8fdf6dba2bd346261cc7f63b
keoki33 <gwong323@yahoo.com>
2019-06-04 08:41:19 +0100

    added test api to tets google action webhooks

--- src/Tfl.js
+++ src/Tfl.js
@@ -3,9 +3,10 @@ import React, { Component } from "react";
 // import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 class Tfl extends Component {
-  state = { Status: [] };
+  state = { Status: [], score: [], chosen: 0 };
 
   componentDidMount() {
+    this.timer = setInterval(() => this.getItems(), 1000);
     fetch("https://api.tfl.gov.uk/Line/dlr/Status?detail=false")
       .then(resp => resp.json())
       .then(x => this.setState({ Status: x, loading: false }));
@@ -15,12 +16,31 @@ class Tfl extends Component {
     return this.state.Status.map(x => <h1>{x.name}</h1>);
   };
 
+  componentWillUnmount() {
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+
+  getItems() {
+    fetch("http://localhost:3000/scores")
+      .then(result => result.json())
+      .then(result =>
+        this.setState({
+          score: result,
+          chosen: result[result.length - 1]["score"]
+        })
+      );
+
+    // console.log(this.state.score[this.state.score.length - 1]["score"]);
+    console.log(this.state);
+  }
+
   render() {
     return (
       <div className="card">
         <h1>TFL</h1>
         <h1>Test deploy</h1>
-        {console.log(this.state)}
+        <h1>Chosen # {this.state.chosen}</h1>
         {this.displayStatus()}
       </div>
     );
