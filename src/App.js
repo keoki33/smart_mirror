622d4dc13cadf676b7713660fde6e950b5110f0b
keoki33 <gwong323@yahoo.com>
2019-06-05 09:03:45 +0100

    css changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
     commandList: [],
     commandValue: "",
     commandKey: "",
-    on: "true"
+    on: "false"
   };
 
   whatever = () => {};
--- src/Calendar.js
+++ src/Calendar.js
@@ -8,11 +8,11 @@ class Calendar extends Component {
   render() {
     return (
       <div className="card">
-        <h1>Calendar</h1>
+        Calendar
         <iframe
           src="https://calendar.google.com/calendar/embed?src=en.uk%23holiday%40group.v.calendar.google.com&ctz=Europe%2FLondon"
-          width="800"
-          height="600"
+          //   width="800"
+          height="800"
           frameborder="0"
           scrolling="no"
         />
