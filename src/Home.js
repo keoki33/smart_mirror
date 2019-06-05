9487d1763a4f37a5c1a8548b6073b7022c6e00dd
keoki33 <gwong323@yahoo.com>
2019-06-05 09:25:11 +0100

    added quote. ready for pitch

--- src/Home.js
+++ src/Home.js
@@ -4,6 +4,7 @@ import Google from "./Google";
 import Youtube from "./Youtube";
 import Weather from "./Weather";
 import Calendar from "./Calendar";
+import Quote from "./Quote";
 // import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 class Home extends Component {
@@ -22,6 +23,7 @@ class Home extends Component {
           commandValue={this.props.commandValue}
         />
         <Calendar />
+        <Quote />
       </div>
     );
   }
---
+++ src/Quote.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Quote extends Component {
+  state = {};
+
+  whatever = () => {};
+
+  render() {
+    return (
+      <div className="quote">
+        Quote:
+        <img src="https://cataas.com/cat/says/ I will see you in hell" alt="" />
+      </div>
+    );
+  }
+}
+
+export default Quote;
