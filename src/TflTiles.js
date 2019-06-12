f52beb9a3a801c01253f28af8dbb2d9545862046
keoki33 <gwong323@yahoo.com>
2019-06-12 12:42:38 +0100

    tfl table grid

--- src/TflTiles.js
+++ src/TflTiles.js
@@ -18,16 +18,20 @@ class TflTiles extends Component {
 
   render() {
     return (
-      <div>
-        <img
-          src={require(`./images/${this.props.line.name}.png`)}
-          alt=""
-          width="15"
-          height="15"
-        />{" "}
-        {this.props.line.name}:{" "}
-        {this.props.line["lineStatuses"][0]["statusSeverityDescription"]}{" "}
-        {this.showIcon()}
+      <div className="tflrows">
+        <div className="t1">
+          <img
+            src={require(`./images/${this.props.line.name}.png`)}
+            alt=""
+            width="15"
+            height="15"
+          />{" "}
+        </div>
+        <div className="t2">{this.props.line.name}: </div>
+        <div className="t3">
+          {this.props.line["lineStatuses"][0]["statusSeverityDescription"]}{" "}
+        </div>
+        <div className="t4">{this.showIcon()}</div>
         {/* {console.log(this.props.line)}
         {console.log(
           this.props.line["lineStatuses"][0]["statusSeverityDescription"]
